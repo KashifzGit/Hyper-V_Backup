a82c050ee53502109c19784aea28e533f8d52ec4
Syed Kashif Ejaz <kashif.ejaz@gmail.com>
2025-09-23 12:13:31 +0500

    Create backup.ps1

---
+++ backup.ps1
@@ -0,0 +1,53 @@
+####### Syed Kashif Ejaz
+####### https://github.com/KashifzGit
+####### Special Thanks to Matt Hansen https://github.com/matthansen0 who wrote the initial version
+####### Last Updated September 23, 2025
+####### This script will export and compress all Hyper-V VMs to a defined folder
+#Variables
+$date = Get-Date -Format "MM-dd-yyyy"
+$base = "C:\backup\backup"
+$logfile = "hyperv-export-job.log"
+$log = $base + $logfile
+
+# Log file entries
+$logexists = Test-Path $log
+If ($logexists -eq $False) {
+    New-Item -ItemType File -Path $base -Name $logfile
+}
+
+Start-Transcript -Append $log
+
+# Read in all VMs
+$vms = Get-VM | ForEach-Object -MemberName Name
+
+# Take new weekly snapshots
+foreach ($vm in $vms){
+    $backupfolder = $vm + $date
+    $fullpath = $base + $backupfolder
+    
+    #Export VM(s)
+    $exportJob = Export-VM -Name $vm -Path $fullpath -AsJob
+
+    # Wait for the Export-VM job to complete before moving on
+    $exportJob | Wait-Job
+    Write-Host -ForegroundColor Green "$vm export complete"
+
+    <# 7z the folder with specified options
+    archive format: 7z
+    compression level: fastest
+    compression method: lzma2
+    dictionary size: 64 KB
+    word size: 32
+    solid block size: 16 MB #>
+    $zipPath = "$fullpath.7z"
+    & 'C:\Program Files\7-Zip\7z.exe' a -t7z -mx=1 -m0=lzma2 -md=64k -mfb=32 -ms=16m $zipPath $fullpath\*
+    Write-Host -ForegroundColor Green "$vm compress to 7z complete"
+
+    # Delete the unzipped folder
+    Remove-Item -Path $fullpath -Recurse
+    Write-Host -ForegroundColor Green "$vm export cleanup complete"
+}
+
+Stop-Transcript
+
+# Notification logic goes here, if desired.
